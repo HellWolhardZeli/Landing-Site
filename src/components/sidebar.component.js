5febed6dede4b744353810883b2f6e8cb9867ce0
Mashu Ajmera <mashuajmera@gmail.com>
2020-06-17 20:50:37 +0530

    mailchimp subscription and category and tag links added

--- src/components/sidebar.component.js
+++ src/components/sidebar.component.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import firebase from "../config/database";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export default class Sidebar extends Component {
+  state = { blog: undefined, email: undefined };
+
+  componentDidMount() {
+    var firestore = firebase.firestore();
+    firestore
+      .collection("blog")
+      .limit(4)
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        this.setState({ blog: data }); // array of cities objects
+      })
+      .catch((err) => console.log(err));
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div class="blog_right_sidebar">
-        <aside class="single_sidebar_widget search_widget">
+        {/* <aside class="single_sidebar_widget search_widget">
           <form action="#">
             <div class="form-group">
               <div class="input-group mb-3">
@@ -29,110 +50,98 @@ export default class Sidebar extends Component {
               Search
             </button>
           </form>
-        </aside>
+        </aside> */}
 
         <aside class="single_sidebar_widget post_category_widget">
           <h4 class="widget_title">Category</h4>
           <ul class="list cat-list">
             <li>
-              <a href="#" class="d-flex">
-                <p>Magazine Issue 5</p>
-                <p>(2)</p>
+              <a href="/blog?category=Issue-5" class="d-flex">
+                <p>Magazine Issue 5 </p>
+                <p> (1)</p>
               </a>
             </li>
             <li>
-              <a href="#" class="d-flex">
-                <p>Magazine Issue 3</p>
-                <p>(1)</p>
+              <a href="/blog?category=Issue-3" class="d-flex">
+                <p>Magazine Issue 3 </p>
+                <p> (1)</p>
               </a>
             </li>
           </ul>
         </aside>
 
-        <aside class="single_sidebar_widget popular_post_widget">
-          <h3 class="widget_title">Recent Posts</h3>
-          <div class="media post_item">
-            <img src={require("../static/img/post/post_1.png")} alt="post" />
-            <div class="media-body">
-              <a href="single-blog.html">
-                <h3>From life was you fish...</h3>
-              </a>
-              <p>January 12, 2019</p>
-            </div>
-          </div>
-          <div class="media post_item">
-            <img src={require("../static/img/post/post_2.png")} alt="post" />
-            <div class="media-body">
-              <a href="single-blog.html">
-                <h3>The Amazing Hubble</h3>
-              </a>
-              <p>02 Hours ago</p>
-            </div>
-          </div>
-          <div class="media post_item">
-            <img src={require("../static/img/post/post_3.png")} alt="post" />
-            <div class="media-body">
-              <a href="single-blog.html">
-                <h3>Astronomy Or Astrology</h3>
-              </a>
-              <p>03 Hours ago</p>
-            </div>
-          </div>
-          <div class="media post_item">
-            <img src={require("../static/img/post/post_4.png")} alt="post" />
-            <div class="media-body">
-              <a href="single-blog.html">
-                <h3>Asteroids telescope</h3>
-              </a>
-              <p>01 Hours ago</p>
-            </div>
-          </div>
-        </aside>
+        {this.state.blog && (
+          <aside class="single_sidebar_widget popular_post_widget">
+            <h3 class="widget_title">Recent Posts</h3>
+            {this.state.blog.map((article, i) => (
+              <div class="media post_item" key={i}>
+                <img
+                  src={require(`../static/img/${article.featuredImage}`)}
+                  alt="post"
+                />
+                <div class="media-body">
+                  <a href={`/blog/${article.slug}`}>
+                    <h3>{article.title}</h3>
+                  </a>
+                  <p>{article.date}</p>
+                </div>
+              </div>
+            ))}
+          </aside>
+        )}
         <aside class="single_sidebar_widget tag_cloud_widget">
           <h4 class="widget_title">Tag Clouds</h4>
           <ul class="list">
-            <li>
-              <a href="#">positivity</a>
-            </li>
-            <li>
-              <a href="#">cheerful</a>
-            </li>
-            <li>
-              <a href="#">joy</a>
-            </li>
-            <li>
-              <a href="#">kind</a>
-            </li>
-            <li>
-              <a href="#">optimistic</a>
-            </li>
+            {["positivity", "cheerful", "joy", "kind", "optimistic"].map(
+              (tag, i) => (
+                <li key={i}>
+                  <a href={`/blog?tag=${tag}`}>{tag}</a>
+                </li>
+              )
+            )}
           </ul>
         </aside>
 
         <aside class="single_sidebar_widget newsletter_widget">
           <h4 class="widget_title">Newsletter</h4>
-
-          <form action="#">
-            <div class="form-group">
-              <input
-                type="email"
-                class="form-control"
-                onfocus="this.placeholder = ''"
-                onblur="this.placeholder = 'Enter email'"
-                placeholder="Enter email"
-                required
-              />
-            </div>
-            <button
-              class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
-              type="submit"
-            >
-              Subscribe
-            </button>
-          </form>
+          <MailchimpSubscribe
+            url={process.env.REACT_APP_MAILCHIMP_URLS}
+            // render={({ subscribe, status, message }) => (
+            //   <form
+            //     onSubmit={(formData) => {
+            //       formData.preventDefault();
+            //       subscribe(formData);
+            //     }}
+            //   >
+            //     <div
+            //       class="genric-btn success circle mb-3"
+            //       dangerouslySetInnerHTML={{ __html: message }}
+            //     ></div>
+            //     <div class="form-group">
+            //       <input
+            //         type="email"
+            //         name="email"
+            //         class="form-control"
+            //         onfocus="this.placeholder = ''"
+            //         onblur="this.placeholder = 'Enter email'"
+            //         placeholder="Enter email"
+            //         onChange={this.handleChange}
+            //         required
+            //       />
+            //     </div>
+            //     <button
+            //       class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+            //       type="submit"
+            //     >
+            //       Subscribe
+            //     </button>
+            //   </form>
+            // )
+            // }
+          />
         </aside>
 
-        <aside class="single_sidebar_widget instagram_feeds">
+        {/* <aside class="single_sidebar_widget instagram_feeds">
           <h4 class="widget_title">Instagram Feeds</h4>
           <ul class="instagram_row flex-wrap">
             <li>
@@ -190,7 +199,7 @@ export default class Sidebar extends Component {
               </a>
             </li>
           </ul>
-        </aside>
+        </aside> */}
       </div>
     );
   }
